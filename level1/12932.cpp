a0f800e7e996dc974667b77c50b201390dadf5aa
areyoureadyforsomeaction <53049011+startedourmission@users.noreply.github.com>
2023-08-16 16:23:43 +0900

    자연수 뒤집어 배열로 만들기

---
+++ level1/12932.cpp
@@ -0,0 +1,16 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/12932
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    while(n >= 10){
+        answer.push_back(n%10);
+        n /= 10;
+    }
+    answer.push_back(n);
+    return answer;
+}
